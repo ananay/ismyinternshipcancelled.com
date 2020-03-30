ccb55fd6853accbe4687ddf2668fc4c7803d9132
ananay <i@ananayarora.com>
2020-03-30 01:30:10 -0700

    Added helmet

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from "gatsby"
+import Helmet from 'react-helmet';
+
 export default class Header extends React.Component {
 
     constructor(props) {
@@ -9,6 +11,17 @@ export default class Header extends React.Component {
     render() {
         return (
             <div className={"header"}>
+                <Helmet>
+                    <title>Is my internship cancelled?</title>
+                    <meta name="title" content="Is my internship cancelled?" />
+                    <meta name="description" content="Is your internship cancelled? Find out about it here with our live tracker." />
+                    <meta name="keywords" content="live, tracking, internship, intern, cancelled, covid, coronavirus, covid19" />
+                    <meta name="robots" content="index, follow" />
+                    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+                    <meta name="language" content="English" />
+                    <meta name="revisit-after" content="1 days" />
+                    <meta name="author" content="Ananay Arora" />
+                </Helmet>
                 <div className={"menu"}>
                     <ul>
                         <Link to="/"><li className={this.props.current == 'home' ? "active" : ""}>Home</li></Link>
