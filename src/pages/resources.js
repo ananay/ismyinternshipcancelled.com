3b9dcd82322165ade2ac41bd6b38d78fec91701b
Kaan Aksoy <kaanaksoyaz@gmail.com>
2020-03-31 15:32:47 -0700

    Restyle Resources page

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -11,22 +11,26 @@ export default class Resources extends React.Component {
     render() {
         return (
             <div>
-                <Header current={"contact"} />
+                <Header current={"resources"} />
                 <center>
                     <div className={"page"}>
                         <h1 className={"page_title"}>Resources</h1>
                         <br />
-                        <h2>It's a tough time. Here are a few resources to help you if you have a cancelled internship.</h2>
+                        <p className={"page_subtitle"}>
+                            It's a tough time. Here are a few resources to help you if you have a cancelled internship.
+                        </p>
+                        <br />
+                        <ul className={"resources"}>
+                            <li>1. <a href="https://summerofcode.withgoogle.com" target="_blank">Google Summer of Code</a></li>
+                            <li>2. <a href="https://acadium.com/" target="_blank">Acadium</a> - a marketplace for remote internships</li>
+                        </ul>
+                        <br />
                         <br />
                         <h2>
-                            <ul className={"resources"}>
-                                <li>1. <a href="https://summerofcode.withgoogle.com" target="_blank">Google Summer of Code</a></li>
-                                <li>2. <a href="https://acadium.com/" target="_blank">Acadium</a> - a marketplace for remote internships</li>
-                            </ul>
+                            <a href="https://docs.google.com/forms/d/e/1FAIpQLScmLlrZOhK46BSuRxAqpOUQW-CgE9gWv_6QFedVKkB74ZKUcg/viewform" target="_blank">
+                                Have anything to contribute to this list? Fill out this form!
+                            </a>
                         </h2>
-                        <br />
-                        <br />
-                        <h2><a href="https://docs.google.com/forms/d/e/1FAIpQLScmLlrZOhK46BSuRxAqpOUQW-CgE9gWv_6QFedVKkB74ZKUcg/viewform" target="_blank">Have anything to contribute to this list? Fill out this form!</a></h2>
                     </div>
                 </center>
             </div>
