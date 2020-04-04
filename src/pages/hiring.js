6027852e9fbbb8325b564cf9e3eec1ef8e5f90ca
ananay <i@ananayarora.com>
2020-04-04 03:49:25 -0700

    fixed link

--- src/pages/hiring.js
+++ src/pages/hiring.js
@@ -17,7 +17,7 @@ export default class Hiring extends React.Component {
                     <div className={"activelyHiring__page"}>
                         <h1>🚨 Are you actively hiring? We want to know. 🚨 </h1>
                         <br /><br />
-                        <p><a href="https://techcrunch.com/2020/03/18/yelp-cancels-internship-program-due-to-covid-19-outbreak/">CloudFlare recently decided to double the internship class of 2020</a> and we absolutely appreciate that, and want to honor those companies that are doing so.</p>
+                        <p><a href="https://techcrunch.com/2020/04/02/cloudflare-ceo-pledges-to-double-2020-internship-class/">CloudFlare recently decided to double the internship class of 2020</a> and we absolutely appreciate that, and want to honor those companies that are doing so.</p>
                         <br />
                         <p>If you're hiring – no matter what country you're in or if your internship is remote – please help out those that have lost their internships during this crisis.</p>
                         <br />
